2034787e7432e56f29ceef4bb3f96f1352a8a65e
sergey <sergey@sergeys-mbp>
2017-05-14 16:15:44 -0700

    @Resource

--- springAnnotations/src/main/java/springAnnotations/Logger.java
+++ springAnnotations/src/main/java/springAnnotations/Logger.java
@@ -1,40 +1,46 @@
 package springAnnotations;
 
-import java.security.GeneralSecurityException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ImportResource;
 
-import spring.*;
+import javax.annotation.Resource;
 
 public class Logger {
-	
+
+	// @Autowired
 	private ConsoleWritter consoleWritter;
 	private FileWritter fileWritter;
 
-	public Logger () {
-		
+	public Logger() {
+
 	}
+
 	
-	public Logger (ConsoleWritter consoleWritter, FileWritter fileWritter) {
+	public Logger(ConsoleWritter consoleWritter, FileWritter fileWritter) {
 		this.consoleWritter = consoleWritter;
 		this.fileWritter = fileWritter;
 	}
 	
+	@Resource
 	public void setConsoleWritter(ConsoleWritter writer) {
+		System.out.println("setConsoleWritter called");
 		this.consoleWritter = writer;
 	}
 
-	public void setFileWritter(FileWritter fileWritter) {
-		this.fileWritter = fileWritter;
+	@Resource
+	public void setFileWritter(Writter fileWritter) {
+		System.out.println("text from Logger fileWritter: " + fileWritter);
+		this.fileWritter = (FileWritter) fileWritter;
 	}
-	
+
 	public void writeFile(String text) {
-		
+		System.out.println("text from logger writeFile: " + text);
 	}
-	
+
 	public void writeConsole(String text) {
-		
 		System.out.println("text from Logger writeConsole: " + text);
 		consoleWritter.write("try write text");
-
 	}
 
 }
